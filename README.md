c8c97ca532c8562bc5a2f74b11edffe38bf07358
Duong Vi Phat <viphat@gmail.com>
2022-03-28 20:00:39 +0700

    Step 1: Best Practices For Application Deployment

--- README.md
+++ README.md
@@ -6,5 +6,6 @@ This is a Flask application that lists the latest articles within the cloud-nati
 
 To run this application there are 2 steps required:
 
-1. Initialize the database by using the `python init_db.py` command. This will create or overwrite the `database.db` file that is used by the web application.
-2.  Run the TechTrends application by using the `python app.py` command. The application is running on port `3111` and you can access it by querying the `http://127.0.0.1:3111/` endpoint.
+1. Setup Dependencies: `pip install -r requirements.txt`
+2. Initialize the database by using the `python init_db.py` command. This will create or overwrite the `database.db` file that is used by the web application.
+3.  Run the TechTrends application by using the `python app.py` command. The application is running on port `3111` and you can access it by querying the `http://127.0.0.1:3111/` endpoint.
--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import sqlite3
+import logging
 
 from flask import Flask, jsonify, json, render_template, request, url_for, redirect, flash
 from werkzeug.exceptions import abort
 
+db_connection_count = 0
+
 # Function to get a database connection.
 # This function connects to database with the name `database.db`
 def get_db_connection():
+    global db_connection_count
     connection = sqlite3.connect('database.db')
     connection.row_factory = sqlite3.Row
+    db_connection_count += 1
     return connection
 
 # Function to get a post using its ID
@@ -20,9 +25,9 @@ def get_post(post_id):
 
 # Define the Flask application
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'your secret key'
+app.config['SECRET_KEY'] = 'LSt!88Q4zW&oR4Z3Pu#jpZwxkyXcGq74'
 
-# Define the main route of the web application 
+# Define the main route of the web application
 @app.route('/')
 def index():
     connection = get_db_connection()
@@ -30,22 +35,25 @@ def index():
     connection.close()
     return render_template('index.html', posts=posts)
 
-# Define how each individual article is rendered 
+# Define how each individual article is rendered
 # If the post ID is not found a 404 page is shown
 @app.route('/<int:post_id>')
 def post(post_id):
     post = get_post(post_id)
     if post is None:
+      app.logger.info('Article with ID "%s" Not Found!', post_id)
       return render_template('404.html'), 404
     else:
+      app.logger.info('Article "%s" retrieved!', post['title'])
       return render_template('post.html', post=post)
 
 # Define the About Us page
 @app.route('/about')
 def about():
+    app.logger.info('About Us retrieved!')
     return render_template('about.html')
 
-# Define the post creation functionality 
+# Define the post creation functionality
 @app.route('/create', methods=('GET', 'POST'))
 def create():
     if request.method == 'POST':
@@ -60,11 +68,38 @@ def create():
                          (title, content))
             connection.commit()
             connection.close()
+            app.logger.info('Article "%s" is created!', title)
 
             return redirect(url_for('index'))
 
     return render_template('create.html')
+# Define the /healthz endpoint for Health Checks
+@app.route('/healthz')
+def healthcheck():
+    response = app.response_class(
+        response=json.dumps({"result":"OK - healthy"}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
+
+# Define the /metrics endpoint for Health Checks
+@app.route('/metrics')
+def metrics():
+    connection = get_db_connection()
+    post_count = connection.execute('SELECT COUNT(*) FROM posts').fetchone()
+    connection.close()
+
+    response = app.response_class(
+        response=json.dumps({"db_connection_count":db_connection_count,"post_count": post_count[0]}),
+        status=200,
+        mimetype='application/json'
+    )
+
+    return response
 
 # start the application on port 3111
 if __name__ == "__main__":
-   app.run(host='0.0.0.0', port='3111')
+    logging.basicConfig(level=logging.DEBUG)
+    app.run(host='0.0.0.0', port='3111')
